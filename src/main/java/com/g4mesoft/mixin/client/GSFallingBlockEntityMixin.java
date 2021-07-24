bbec44230a4741b70e6643d141c8422a9d9bbc66
G4me4u <G4me4u@gmail.com>
2021-07-24 15:11:12 +0200

    Fixed issue where sand might be removed on the client after the sand has been placed on the server.

--- src/main/java/com/g4mesoft/mixin/client/GSFallingBlockEntityMixin.java
+++ src/main/java/com/g4mesoft/mixin/client/GSFallingBlockEntityMixin.java
@@ -1,6 +1,8 @@
 package com.g4mesoft.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.g4mesoft.core.GSCoreOverride;
 import com.g4mesoft.core.client.GSClientController;
@@ -10,6 +12,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.entity.MovementType;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
@@ -20,6 +23,18 @@ public abstract class GSFallingBlockEntityMixin extends Entity {
 		super(type, world);
 	}
 
+	@Redirect(method = "tick", at = @At(value = "INVOKE", ordinal = 0,
+	          target = "Lnet/minecraft/world/World;removeBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"))
+	private boolean redirectTickRemoveBlock(World world, BlockPos pos, boolean move) {
+		if (!world.isClient || GSClientController.getInstance().getTpsModule().sPrettySand.getValue() == GSTpsModule.PRETTY_SAND_DISABLED) {
+			// Do not remove the source block on the client when pretty sand is
+			// enabled. This might cause the client to remove the final position,
+			// if it is lagging behind, and desync the server and client world.
+			return world.removeBlock(pos, move);
+		}
+		return false;
+	}
+	
 	@GSCoreOverride
 	@Override
 	public void move(MovementType movementType, Vec3d movement) {
