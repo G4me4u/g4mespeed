42c19b638ad25a3298d58acf355193c2eb0b65e2
G4me4u <G4me4u@gmail.com>
2022-12-13 22:10:19 +0100

    Added a way to register setting panels (for use in the setting GUI), and bumped version to 1.3.4

--- src/main/java/com/g4mesoft/setting/decoder/GSFloatSettingDecoder.java
+++ src/main/java/com/g4mesoft/setting/decoder/GSFloatSettingDecoder.java
@@ -1,5 +1,6 @@
 package com.g4mesoft.setting.decoder;
 
+import com.g4mesoft.setting.GSISettingDecoder;
 import com.g4mesoft.setting.types.GSFloatSetting;
 
 import net.minecraft.network.PacketByteBuf;
