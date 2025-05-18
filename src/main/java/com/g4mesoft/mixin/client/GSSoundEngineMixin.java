eb6bc9a1e30082fda26410542f37170d233a4b9c
G4me4u <G4me4u@gmail.com>
2025-05-18 14:57:14 +0200

    Fixed pitch restriction from 0.5 to 2.0 to be unlimited as in other versions.

--- src/main/java/com/g4mesoft/mixin/client/GSSoundEngineMixin.java
+++ src/main/java/com/g4mesoft/mixin/client/GSSoundEngineMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.g4mesoft.core.client.GSClientController;
 import com.g4mesoft.module.tps.GSITpsDependant;
@@ -20,7 +19,6 @@ import com.g4mesoft.setting.GSSettingCategory;
 
 import net.minecraft.client.sound.instance.SoundInstance;
 import net.minecraft.client.sound.system.SoundEngine;
-import net.minecraft.util.math.MathHelper;
 
 @Mixin(SoundEngine.class)
 public abstract class GSSoundEngineMixin implements GSITpsDependant, GSISettingChangeListener {
@@ -44,26 +42,10 @@ public abstract class GSSoundEngineMixin implements GSITpsDependant, GSISettingC
 		gs_tpsModule.addTpsListener(this);
 		client.getSettingManager().addChangeListener(this);
 	}
-	
-	@Inject(
-		method = "getPitch",
-		cancellable = true,
-		at = @At("HEAD")
-	)
-	private void onGetAdjustedPitch(SoundInstance soundInstance, CallbackInfoReturnable<Float> cir) {
-		float pitch = MathHelper.clamp(soundInstance.getPitch(), 0.5f, 2.0f);
-		
-		if (gs_tpsModule.cShiftPitch.get()) {
-			// Scale pitch by relative tps difference to the default.
-			pitch *= gs_tpsModule.getTps() / GSTpsModule.DEFAULT_TPS;
-		}
-
-		cir.setReturnValue(pitch);
-		cir.cancel();
-	}
 
 	@Unique
 	private void updatePitch() {
+		// Note: pitch is updated accordingly inside of each source.
 		for (Map.Entry<SoundInstance, String> soundEntry : channelsByEvent.entrySet()) {
 			float pitch = getPitch(soundEntry.getKey());
 			system.setPitch(soundEntry.getValue(), pitch);
---
+++ src/main/java/com/g4mesoft/mixin/client/GSSourceOpenALMixin.java
@@ -0,0 +1,54 @@
+package com.g4mesoft.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.g4mesoft.core.client.GSClientController;
+import com.g4mesoft.module.tps.GSTpsModule;
+
+import net.minecraft.unmapped.C_7775554;
+import paulscode.sound.SoundBuffer;
+import paulscode.sound.Source;
+
+@Mixin(C_7775554.class)
+public class GSSourceOpenALMixin extends Source {
+
+	public GSSourceOpenALMixin(Source old, SoundBuffer soundBuffer) {
+		super(old, soundBuffer);
+	}
+
+	@Unique
+	private GSTpsModule gs_tpsModule;
+	
+	@Inject(
+		method = "<init>",
+		at = @At("RETURN")
+	)
+	private void onInit(CallbackInfo ci) {
+		gs_tpsModule = GSClientController.getInstance().getTpsModule();
+	}
+	
+	@Inject(
+		method = "setPitch",
+		at = @At(
+			value = "INVOKE",
+			shift = Shift.AFTER,
+			target =
+				"Lpaulscode/sound/Source;setPitch(" +
+					"F" +
+				")V"
+		)
+	)
+	public void onSetPitch(float newPitch, CallbackInfo ci) {
+		// Note: pitch is generally clamped between 0.5 and 2.0, but when
+		//       using OpenAL this restriction is not required.
+		if (gs_tpsModule.cShiftPitch.get()) {
+			// Scale pitch by relative tps difference to the default.
+			this.pitch = newPitch * gs_tpsModule.getTps() / GSTpsModule.DEFAULT_TPS;
+		}
+	}
+}
